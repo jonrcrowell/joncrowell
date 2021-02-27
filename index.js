68e3aa7ef82f6f2919a05daf63bafab081d9b8d1
Jon Crowell <jon.crowell@stonegiantstudio.com>
2021-02-27 00:31:35 -0600

    Add formatting and use cli-welcome

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
+const pkgJson = require("./package.json");
+const welcome = require("cli-welcome");
 
+welcome({
+  title: pkgJson.name,
+  tagLine: `Buenos ding-dong-diddly-dias, neighborino. We are so glad you're here.`,
+  description: pkgJson.description,
+  version: pkgJson.version,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: false,
+});
 console.log(`
 
 Jon Crowell
 
 Software engineer focused on building interactive apps using JavaScript, primarily with React.
 
-`)
+`);
